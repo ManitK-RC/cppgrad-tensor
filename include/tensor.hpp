a5ba15cf4a20181eea0880c1ab39436748f5e8dd
Manit Kaushik <Manitkaushik24@gmail.com>
2025-10-16 23:00:31 +0530

    added constructors

--- include/tensor.hpp
+++ include/tensor.hpp
@@ -3,10 +3,10 @@
 #include <memory>
 #include <cstddef>
 #include <type_traits> 
+#include <new>
+#include <utility>
 
-// attributes - shape, dtype, device, requires_grad
-// storing the tensor in a 1d format
-// there is a pointer to data stored in the tensor class
+// constructor - random
 
 // reason for enum class is that its just 1 byte of memory compared to a string which is 24 bytes
 enum class DType: uint8_t {
@@ -36,42 +36,69 @@ constexpr DType get_dtype() {
 template <typename T>
 class Tensor{
     public:
-        // constructor that takes shape, device and requires_grad and allocates memory for data
+        // CONSTRUCTORS
+        // constructor fills zeroes
         Tensor(const std::vector<size_t>& shape, Device device = Device::CPU, bool requires_grad = false): shape_(shape), device_(device), requires_grad_(requires_grad), dtype_(get_dtype<T>()) {
-            size_t n = 1;
-            for (auto dim : shape_) n *= dim;
-            data_ = std::make_unique<T[]>(n);
+            size_ = 1;
+            for (auto dim : shape_) size_ *= dim;
+            data_ = std::make_unique<T[]>(size_); // by default initializes each element to zero
         }
 
-        // constructor that takes shape, data, device and requires_grad and allocates memory for data and moves the data
+        // empty constructor & no memory allocation
+        Tensor(const std::vector<size_t>& shape, std::nullptr_t, Device device = Device::CPU, bool requires_grad = false)
+            : shape_(shape), device_(device), requires_grad_(requires_grad), dtype_(get_dtype<T>()) 
+        {
+            size_ = 1;
+            for (auto dim : shape_) size_ *= dim;
+            data_ = nullptr;
+        }
+
+        // constructor fills 'num' value
+        Tensor(const std::vector<size_t>& shape, T num, Device device = Device::CPU, bool requires_grad = false): shape_(shape), device_(device), requires_grad_(requires_grad), dtype_(get_dtype<T>()) {
+            size_ = 1;
+            for (auto dim : shape_) size_ *= dim;
+            data_ = std::make_unique<T[]>(size_);
+            std::fill(data_.get(), data_.get() + size_, num);
+        }
+
+        // constructor fills by vector
         Tensor(const std::vector<size_t>& shape, std::vector<T>& data, Device device = Device::CPU, bool requires_grad = false): shape_(shape), device_(device), requires_grad_(requires_grad), dtype_(get_dtype<T>()) {
-            size_t n = 1;
-            for (auto dim : shape_) n *= dim;
-            if (n != data.size()) {
+            size_ = 1;
+            for (auto dim : shape_) size_ *= dim;
+            if (size_ != data.size()) {
                 throw std::invalid_argument("Invalid Shape");
             }
-            data_ = std::make_unique<T[]>(n);
-            std::move(data.begin(), data.end(), data_.get()); // using move instead of copy because 
+            data_ = std::make_unique<T[]>(size_);
+            std::move(data.begin(), data.end(), data_.get()); // using move instead of copy
         }
 
+        // constructor fills by initializer_list
         Tensor(const std::vector<size_t>& shape, std::initializer_list<T> data, Device device = Device::CPU, bool requires_grad = false): shape_(shape), device_(device), requires_grad_(requires_grad), dtype_(get_dtype<T>()) {
-            size_t n = 1;
-            for (auto dim : shape_) n *= dim;
-            if (n != data.size()) {
+            size_ = 1;
+            for (auto dim : shape_) size_ *= dim;
+            if (size_ != data.size()) {
                 throw std::invalid_argument("Invalid Shape");
             }
-            data_ = std::make_unique<T[]>(n);
+            data_ = std::make_unique<T[]>(size_);
             std::copy(data.begin(), data.end(), data_.get()); // have to use copy here because initializer_list does not support move 
         }
 
-        // accessors
+        // ATTRIBUTES
+        // get shape of the tensor
         const std::vector<size_t>& shape() const { return shape_; }
+
+        // get size of the tensor
+        std::size_t size() const { return size_; }
+
+        // get the device of the tensor
         std::string device() const {
             switch(device_) {
                 case Device::CPU: return "CPU";
                 case Device::CUDA: return "CUDA";
             }
         }
+
+        // get the dtype of the tensor
         std::string dtype() const {
             switch(dtype_) {
                 case DType::FLOAT32: return "FLOAT32";
@@ -81,15 +108,32 @@ class Tensor{
                 case DType::BOOL: return "BOOL";
             }
         }
+
+        // check if tensor requires grad or not
         bool requires_grad() const { return requires_grad_; }
-        const T* data() const { return data_.get(); } // this returns a pointer to the data
+
+        // get the data pointer of the tensor
+        T* data_ptr() const { return data_.get(); }
+
+        // DATA MANIPULATION - TO DO
 
 
     private:
         std::unique_ptr<T[]> data_;
         std::vector<size_t> shape_;
+        std::size_t size_;
         DType dtype_;
         Device device_;
         bool requires_grad_;
     };
+
+// get the data of the tensor as a stream
+template<typename T>
+std::ostream& operator<<(std::ostream &os, const Tensor<T> &tensor){
+    for(size_t i=0; i<tensor.size(); i++){
+        os << tensor.data_ptr()[i] << " ";
+    }
+    return os;
+}
+
 };
--- test.cpp
+++ test.cpp
@@ -5,20 +5,40 @@ using namespace std;
 using namespace cppgrad;
 
 int main() {
+    // CONSTRUCTORS
+
+    // 1. filled with 0s
+    Tensor<float> zero_tensor({2, 3});
+    cout<<zero_tensor<<endl;
+
+    // 2. empty tensor of int32
+    Tensor<int32_t> empty_tensor({3, 2, 2}, nullptr);
+    cout<<empty_tensor.data_ptr()<<endl;
+    
+    // 3. filled with a specific number
+    Tensor<float> filled_tensor({2, 2}, 3.14);
+    cout<<filled_tensor<<endl;
+
+    // 4. filled with data from vector
     std::vector<float> data = {1, 2, 3, 4};
     Tensor<float> tensor1({2,2}, data);
+    cout<<tensor1<<endl;
 
-    cout<<tensor1.data()<<endl;
+    // 5. filled with data from initializer list
+    Tensor<float> tensor2({2, 2}, {5, 6, 7, 8});
+    cout<<tensor2<<endl;
+
+    // ATTRIBUTES
+    cout<<tensor1.data_ptr()<<endl;
+    cout<<tensor1.size()<<endl;
     vector<size_t> shape = tensor1.shape();
     for(auto dim : shape) cout<<dim<<" ";
-    cout<<endl;
     cout<<tensor1.requires_grad()<<endl;
     cout<<tensor1.device()<<endl;
     cout<<tensor1.dtype()<<endl;
-    Tensor<float> tensor2({2, 2}, {5, 6, 7, 8});
-    cout<<tensor2.data()<<endl;
-    shape = tensor2.shape();
-    for(auto dim : shape) cout<<dim<<" ";
-    cout<<endl;
+
+    // DATA MANIPULATION - TO DO
+
+    
     return 0;
 }
